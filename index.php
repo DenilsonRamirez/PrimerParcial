c4d2b2fda8747e5d526502919ff3f7aed63e9258
P! <denixmas123@gmail.com>
2022-08-08 19:51:48 -0600

    PrimerParcial

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+</head>
+<body>
+    <div id="menu">
+        <nav>
+            <ul>
+                <li><a href="">Inicio</a></li>
+                <li><a href="tablas.html">Tablas</a></li>
+                <li><a href="">Formularios</a></li>
+            </ul>
+
+        </nav>
+    </div>
+
+    <div class="img">
+        <figure>
+            <img src="/src/laptop.png" alt="img laptop" class="dimensionImg">        
+        </figure>
+        
+    </div>
+
+    <div class="txtimg">
+            <h1 style="color:#00a2e8">Bienvenido</h1>
+                <p>
+                    Al examen parcial de desarrollo Web, en las páginas que usted verá aquí podrá ver el avance que hemos tenido en las primeras semanas de clase...
+                </p>
+    </div>
+    
+
+    <footer>
+        <p>Todos los derechos reservados a: Denilson Ramírez 4490-19-20926</p>
+    </footer>
+    <link rel="stylesheet" href="styles.css">
+</body>
+
+</html>
